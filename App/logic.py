2dcdf7a62059ac68966b217485e82b8901aa57c5
Archilipeper <p.archila@uniandes.edu.co>
2025-03-29 10:49:57 -0500

    Carga de datso e imports en logic y imports en view

--- App/logic.py
+++ App/logic.py
@@ -1,11 +1,35 @@
 import time
+import csv
+from DataStructures.List import array_list as ar
+from DataStructures.List import single_linked_list as sl
+from DataStructures.Queue import queue as q
+from DataStructures.Stack import stack as st
+from DataStructures.Map import map_linear_probing as lp
+from DataStructures.Map import map_separate_chaining as sc
+
+csv.field_size_limit(2147483647)
 
 def new_logic():
     """
     Crea el catalogo para almacenar las estructuras de datos
     """
-    #TODO: Llama a las funciónes de creación de las estructuras de datos
-    pass
+    #TODO: Llama a las funciónes de creación de las estructuras de datos 
+    #HECHO
+    catalog = lp.new_map(11, 0.5, 109345121)
+
+    lp.put(catalog, "source", ar.new_list())
+    lp.put(catalog, "commodity", ar.new_list())
+    lp.put(catalog, "statical_category", ar.new_list())
+    lp.put(catalog, "unit_measurement", ar.new_list())
+    lp.put(catalog, "state_name", ar.new_list())
+    lp.put(catalog, "location", ar.new_list())
+    lp.put(catalog, "year_collection", ar.new_list())
+    lp.put(catalog, "freq_collection", ar.new_list())
+    lp.put(catalog, "reference_period", ar.new_list())
+    lp.put(catalog, "load_time", ar.new_list())
+    lp.put(catalog, "value", ar.new_list())
+
+    return catalog
 
 
 # Funciones para la carga de datos
@@ -15,7 +39,52 @@ def load_data(catalog, filename):
     Carga los datos del reto
     """
     # TODO: Realizar la carga de datos
-    pass
+    with open(filename, mode = "r", encoding='utf-8') as file:
+        start_time = get_time()
+        input_file = csv.DictReader(file)
+        count = 0
+        año_max = 0
+        año_min = 0
+        last_five = sl.new_list()
+        first_five = sl.new_list()
+
+        for x in input_file:
+            ar.add_last(lp.get(catalog, "source"), x["source"])
+            ar.add_last(lp.get(catalog, "commodity"), x["commodity"])
+            ar.add_last(lp.get(catalog, "statical_category"), x["statical_category"])
+            ar.add_last(lp.get(catalog, "unit_measurement"), x["unit_measurement"])
+            ar.add_last(lp.get(catalog, "state_name"), x["state_name"])
+            ar.add_last(lp.get(catalog, "location"), x["location"])
+            ar.add_last(lp.get(catalog, "year_collection"), x["year_collection"])
+            ar.add_last(lp.get(catalog, "freq_collection"), x["freq_collection"])
+            ar.add_last(lp.get(catalog, "reference_period"), x["reference_period"])
+            ar.add_last(lp.get(catalog, "load_time"), x["load_time"])
+            ar.add_last(lp.get(catalog, "value"), x["value"])
+
+            if int(x["year_collection"]) > año_max or año_max == 0:
+                año_max = int(x["year_collection"])
+            if int(x["year_collection"]) < año_min or año_min == 0:
+                año_min = int(x["year_collection"])
+            count += 1
+
+            datos_listas = {"year_collection": x["year_collection"],
+                            "load_time": x["load_time"],
+                            "state_name": x["state_name"],
+                            "source": x["source"],
+                            "unit_measurement": x["unit_measurement"],
+                            "value": x["value"]}
+            if count < 5:
+                sl.add_last(first_five, datos_listas)
+
+            elif count >= 5:
+                sl.add_last(last_five, datos_listas)
+                if sl.size(last_five) > 5:
+                    sl.remove_first(first_five)
+        end_time = get_time()
+        delta = delta_time(start_time, end_time)
+        return delta, count, año_min, año_max, last_five, first_five
+
+    
 
 # Funciones de consulta sobre el catálogo
 
